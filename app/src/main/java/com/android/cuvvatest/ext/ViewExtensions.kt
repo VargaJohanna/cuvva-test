5caf5d4983512012f8807a8ee5bc21f1457471a5
VargaJohanna <varga.johanna@gmail.com>
2019-05-10 18:13:37 +0100

    Finish home screen

---
+++ app/src/main/java/com/android/cuvvatest/ext/ViewExtensions.kt
@@ -0,0 +1,19 @@
+package com.android.cuvvatest.ext
+
+import android.view.View
+
+fun View.display(displayed: Boolean) {
+    visibility = if (displayed) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
+}
+
+fun View.show(visible: Boolean) {
+    visibility = if (visible) {
+        View.VISIBLE
+    } else {
+        View.INVISIBLE
+    }
+}
--- app/src/main/java/com/android/cuvvatest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/cuvvatest/ui/home/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.android.cuvvatest.ui.home
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.cuvvatest.R
+import com.android.cuvvatest.ext.show
 import com.android.cuvvatest.model.HomeDataObject
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import org.koin.androidx.viewmodel.ext.viewModel
 
@@ -24,15 +28,21 @@ class HomeFragment : Fragment(), ActivePolicyAdapter.ItemClickListener, VehicleA
         val vehicleAdapter = VehicleAdapter(ArrayList(), this)
         return inflater.inflate(R.layout.fragment_home, container, false).apply {
             generateActiveList(activeAdapter, active_policy_recycler_view)
-            observeActiveList(activeAdapter)
+            observeActiveList(activeAdapter, progress_bar)
             generateVehicleList(vehicleAdapter, vehicles_recycler_view)
-            observeVehicleList(vehicleAdapter)
+            observeVehicleList(vehicleAdapter, progress_bar)
+            swipe_refresh.setOnRefreshListener {
+                //Refresh data here
+                Handler().postDelayed({ swipe_refresh.isRefreshing = false }, 1000)
+            }
         }
     }
 
-    private fun observeVehicleList(vehicleAdapter: VehicleAdapter) {
+    private fun observeVehicleList(vehicleAdapter: VehicleAdapter, progressBar: ProgressBar) {
+        progressBar.show(true)
         homeViewModel.getVehicleList().observe(this, Observer {
             vehicleAdapter.updateList(it)
+            progressBar.show(false)
         })
     }
 
@@ -43,9 +53,11 @@ class HomeFragment : Fragment(), ActivePolicyAdapter.ItemClickListener, VehicleA
         }
     }
 
-    private fun observeActiveList(adapter: ActivePolicyAdapter) {
+    private fun observeActiveList(adapter: ActivePolicyAdapter, progressBar: ProgressBar) {
+        progressBar.show(true)
         homeViewModel.getActiveVehicleList().observe(this, Observer {
             adapter.updateList(it)
+            progressBar.show(false)
         })
     }
 
